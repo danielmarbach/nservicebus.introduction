74ecb3dceec91bb495c627c40adeac802aa55e45
danielmarbach <daniel.marbach@openplace.net>
2012-09-20 21:33:35 +0200

    Added saga not found handler

---
+++ SiriusCyberneticsCorp.Facility/FacilityNotAvailable.cs
@@ -0,0 +1,27 @@
+﻿namespace SiriusCyberneticsCorp.Facility
+{
+    using NServiceBus;
+    using NServiceBus.Saga;
+
+    using SiriusCyberneticsCorp.Contract.Complaint;
+    using SiriusCyberneticsCorp.Contract.Facility;
+
+    public class FacilityNotAvailable : IHandleSagaNotFound
+    {
+        private readonly IBus bus;
+
+        public FacilityNotAvailable(IBus bus)
+        {
+            this.bus = bus;
+        }
+
+        public void Handle(object message)
+        {
+            var msg = message as ComplainedAbout;
+            if (msg != null)
+            {
+                this.bus.Publish<BecameDemotivated>(m => m.FacilityId = msg.FacilityId);
+            }
+        }
+    }
+}
