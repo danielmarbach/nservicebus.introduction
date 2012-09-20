8f3a3d6dcc9cdc3cc007dc6a99c32a6278b06714
danielmarbach <daniel.marbach@openplace.net>
2012-09-20 14:10:21 +0200

    Automatically adjust the console windows

--- SiriusCyberneticsCorp.Complaint.Frontend/Endpoint.cs
+++ SiriusCyberneticsCorp.Complaint.Frontend/Endpoint.cs
@@ -1,5 +1,8 @@
 ﻿namespace SiriusCyberneticsCorp.Complaint.Frontend
 {
+    using System;
+    using System.Runtime.InteropServices;
+
     using NServiceBus;
 
     /// <summary>
@@ -10,8 +13,21 @@
     /// </summary>
     public class Endpoint : IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization, ICanBeMean
     {
+        const int SwpNosize = 0x0001;
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        private static readonly IntPtr ConsolePtr = GetConsoleWindow();
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
+
         public void Init()
         {
+            Console.SetWindowSize(70, 30);
+            SetWindowPos(ConsolePtr, 0, 10, 420, 0, 0, SwpNosize);
+
             Configure.With(AllAssemblies.Except("Raven.Backup.exe"))
                 .DefaultBuilder()
                 .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith("SiriusCyberneticsCorp.InternalMessages.Complaint"))
--- SiriusCyberneticsCorp.Facility/Endpoint.cs
+++ SiriusCyberneticsCorp.Facility/Endpoint.cs
@@ -1,6 +1,7 @@
 ﻿namespace SiriusCyberneticsCorp.Facility
 {
     using System;
+    using System.Runtime.InteropServices;
 
     using NServiceBus;
 
@@ -9,9 +10,22 @@
     /// </summary>
     public class Endpoint : IConfigureThisEndpoint, AsA_Publisher, IWantCustomInitialization
     {
+        const int SwpNosize = 0x0001;
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        private static readonly IntPtr ConsolePtr = GetConsoleWindow();
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
+
         public void Init()
         {
             Console.Title = "Facility";
+
+            Console.SetWindowSize(80, 30);
+            SetWindowPos(ConsolePtr, 0, 650, 10, 0, 0, SwpNosize);
 
             Configure.With()
                 .DefaultBuilder()
