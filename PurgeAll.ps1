4b85f4770938fa264c8c94893fee69decf9cc96e
danielmarbach <daniel.marbach@openplace.net>
2012-09-26 10:34:35 +0200

    Purge script now also includes raven db data (cherry picked from commit 6292718a00fe473201e97ed1181b9afc0123bcca)

--- PurgeAll.ps1
+++ PurgeAll.ps1
@@ -1,3 +1,5 @@
+"Emptying all message queues"
 [Reflection.Assembly]::LoadWithPartialName("System.Messaging")
-
 [System.Messaging.MessageQueue]::GetPrivateQueuesByMachine(".") | % { $_.Purge(); }
+"Wiping out raven db data"
+remove-item **\bin\*\Data -recurse -force
