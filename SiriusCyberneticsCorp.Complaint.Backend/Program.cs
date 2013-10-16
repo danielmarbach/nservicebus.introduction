d85c7f97a878e44ff6e050ee27820989bd9bb424
danielmarbach <daniel.marbach@openplace.net>
2013-10-16 16:48:33 +0200

    Disabled purging on complaint backend

--- SiriusCyberneticsCorp.Complaint.Backend/Program.cs
+++ SiriusCyberneticsCorp.Complaint.Backend/Program.cs
@@ -33,7 +33,7 @@
             Configure.Serialization.Json();
 
             Configure.Instance.UseTransport<Msmq>()
-                .PurgeOnStartup(true);
+                .PurgeOnStartup(false);
 
             Configure.Transactions.Enable();
 
