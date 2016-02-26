7e6334da28d021661c70fd95794023e4e4e10b94
Daniel Marbach <daniel.marbach@particular.net>
2016-02-26 12:05:37 +0100

    Removed window positioning
    # Conflicts:
    #	SiriusCyberneticsCorp.Facility/EndpointConfig.cs
    #	SiriusCyberneticsCorp.Sales.Backend/EndpointConfig.cs

--- SiriusCyberneticsCorp.Complaint.Backend/Program.cs
+++ SiriusCyberneticsCorp.Complaint.Backend/Program.cs
@@ -1,28 +1,14 @@
 ﻿namespace SiriusCyberneticsCorp.Complaint.Backend
 {
     using System;
-    using System.Runtime.InteropServices;
-
     using NServiceBus;
     using NServiceBus.Persistence;
 
     class Program
     {
-        const int SwpNosize = 0x0001;
-
-        [DllImport("kernel32.dll", ExactSpelling = true)]
-        private static extern IntPtr GetConsoleWindow();
-
-        private static readonly IntPtr ConsolePtr = GetConsoleWindow();
-
-        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
-
         static void Main(string[] args)
         {
             Console.Title = "Complaint.Backend";
-            Console.SetWindowSize(80, 30);
-            SetWindowPos(ConsolePtr, 0, 650, 420, 0, 0, SwpNosize);
 
             Console.WriteLine("Complaint Backend starting up...");
 
