d8b1b6b7673749d0ac1021c0953df1a9470ba279
Daniel Marbach <daniel.marbach@particular.net>
2016-02-26 12:05:37 +0100

    Removed window positioning

--- SiriusCyberneticsCorp.Facility/EndpointConfig.cs
+++ SiriusCyberneticsCorp.Facility/EndpointConfig.cs
@@ -12,22 +12,9 @@ namespace SiriusCyberneticsCorp.Facility
     /// </summary>
     public class EndpointConfig : IConfigureThisEndpoint, AsA_Server
     {
-        const int SwpNosize = 0x0001;
-
-        [DllImport("kernel32.dll", ExactSpelling = true)]
-        private static extern IntPtr GetConsoleWindow();
-
-        private static readonly IntPtr ConsolePtr = GetConsoleWindow();
-
-        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
-
         public void Customize(BusConfiguration configuration)
         {
             Console.Title = "Facility";
-
-            Console.SetWindowSize(80, 30);
-            SetWindowPos(ConsolePtr, 0, 650, 10, 0, 0, SwpNosize);
 
             configuration.Conventions()
                 .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith("SiriusCyberneticsCorp.InternalMessages"))
