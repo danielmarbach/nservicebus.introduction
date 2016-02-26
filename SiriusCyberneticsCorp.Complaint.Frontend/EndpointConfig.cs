4eee2ec1983bc48ab8b6a0842c2329b2b27e83bc
Daniel Marbach <daniel.marbach@particular.net>
2016-02-26 12:05:37 +0100

    Removed window positioning

--- SiriusCyberneticsCorp.Complaint.Frontend/EndpointConfig.cs
+++ SiriusCyberneticsCorp.Complaint.Frontend/EndpointConfig.cs
@@ -2,9 +2,6 @@
 
 namespace SiriusCyberneticsCorp.Complaint.Frontend
 {
-    using System;
-    using System.Runtime.InteropServices;
-
     using NServiceBus;
 
     /// <summary>
@@ -15,21 +12,8 @@ namespace SiriusCyberneticsCorp.Complaint.Frontend
     /// </summary>
     public class EndpointConfig : IConfigureThisEndpoint, AsA_Server
     {
-        const int SwpNosize = 0x0001;
-
-        [DllImport("kernel32.dll", ExactSpelling = true)]
-        private static extern IntPtr GetConsoleWindow();
-
-        private static readonly IntPtr ConsolePtr = GetConsoleWindow();
-
-        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
-
         public void Customize(BusConfiguration configuration)
         {
-            Console.SetWindowSize(70, 30);
-            SetWindowPos(ConsolePtr, 0, 10, 420, 0, 0, SwpNosize);
-
             configuration.Conventions()
                 .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.StartsWith("SiriusCyberneticsCorp.InternalMessages.Complaint"))
                 .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith("SiriusCyberneticsCorp.Contract.Facility"));
--- SiriusCyberneticsCorp.Sales.Backend/EndpointConfig.cs
+++ SiriusCyberneticsCorp.Sales.Backend/EndpointConfig.cs
@@ -3,28 +3,15 @@
 namespace SiriusCyberneticsCorp.Sales.Backend
 {
     using System;
-    using System.Runtime.InteropServices;
-
     using NServiceBus;
 
     public class EndpointConfig : IConfigureThisEndpoint, AsA_Server
     {
-        const int SwpNosize = 0x0001;
-        
-        [DllImport("kernel32.dll", ExactSpelling = true)]
-        private static extern IntPtr GetConsoleWindow();
-
-        private static readonly IntPtr ConsolePtr = GetConsoleWindow();
-
-        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
-
         public void Customize(BusConfiguration configuration)
         {
             Console.Title = "Sales.Backend";
 
             Console.SetWindowSize(70, 29);
-            SetWindowPos(ConsolePtr, 0, 10, 10, 0, 0, SwpNosize);
 
             configuration
                 .Conventions()
